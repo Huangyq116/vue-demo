1aef912799b833f7167cd0cc4b9d18ea54ab9789
huangyanqing02 <huangyanqing02@baidu.com>
2022-03-21 09:41:26 +0800

    update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
 
-// module.exports={
-//   devServer:{
-//     port: 8080,
-//     host: localhost,
-//     open: true
-//   }
-// }
+module.exports={
+  devServer:{
+    port: 8080,
+    host: 'localhost',
+    open: true
+  }
+}
